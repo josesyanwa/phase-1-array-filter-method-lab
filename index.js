7091bc4ff71b76efb14e72ff27b951dcc91b4977
Joses Syanwa <josesyanwa@gmail.com>
2023-10-10 10:54:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = [
+    { name: 'John', hometown: 'New York' },
+    { name: 'Jane', hometown: 'Los Angeles' },
+    { name: 'Doe', hometown: 'Chicago' },
+    { name: 'Bob', hometown: 'San Francisco' },
+  ];
+  
+  // Function to find matching drivers by name (case insensitive)
+  function findMatching(driversArray, query) {
+    return driversArray.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  // Function to find drivers whose names start with provided letters
+  function fuzzyMatch(driversArray, query) {
+    return driversArray.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+  
+  // Function to match drivers by name in an array of driver objects
+  function matchName(driverObjectsArray, query) {
+    return driverObjectsArray.filter(driver =>
+      driver.name.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  
